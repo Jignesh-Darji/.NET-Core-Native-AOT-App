e8a2f190434a775207623d805600dddb578275e2
Jignesh Darji <jignesh@zenkins.com>
2024-01-01 18:26:36 +0530

    Initial Commit

---
+++ StateLessClass.cs
@@ -0,0 +1,35 @@
+﻿using Stateless;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET_Core_Console_App
+{
+    internal class StateLessClass
+    {
+        // Define your state and trigger enums
+        enum DoorState { Closed, Open }
+        enum DoorTrigger { ButtonPressed, MotionDetected }
+
+        internal void DoWorkExample()
+        {
+            // Instantiate the state machine with the specific types
+            StateMachine<DoorState, DoorTrigger> doorMachine = new StateMachine<DoorState, DoorTrigger>(DoorState.Closed);
+
+            // Configure the state machine 
+            doorMachine.Configure(DoorState.Closed)
+        .OnEntry(() => Console.WriteLine("Door is closed"))
+        .Permit(DoorTrigger.ButtonPressed, DoorState.Open)
+        .OnEntry(() => Console.WriteLine("Door is open"))
+        .Permit(DoorTrigger.ButtonPressed, DoorState.Closed);
+
+            // ...rest of your code...
+
+            //doorMachine.Fire(DoorTrigger.MotionDetected);
+            
+        }
+
+    }
+}
